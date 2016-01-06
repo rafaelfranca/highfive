9a955b291aa408131081ef7df0ec6d5d4beebc75
Rafael Mendonça França <rafaelmfranca@gmail.com>
2016-01-06 01:22:31 -0200

    Assign the issue to the reviewer if body contains it

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,4 +1,21 @@
 class Bot
+  # Public: creates an instance of the bot.
+  #
+  # client - the GitHub client. Needs to have the same API as octokit.
+  def initialize(client)
+    @client = client
+  end
+
+  # Public: handles a pull request.
+  #
+  # payload - the Hash containing the payload of the request.
   def handle_pull_request!(payload)
+    repository = payload['repository']['full_name']
+    number = payload['number']
+    body = payload['pull_request']['body']
+
+    assignee = /\b[rR]\?[:\- ]*@([a-zA-Z0-9\-_]+)/.match(body)[1]
+
+    @client.update_issue(repository, number, assignee: assignee)
   end
 end
--- test/test_bot.rb
+++ test/test_bot.rb
@@ -1,7 +1,24 @@
 require 'helper'
 
 class BotTest < Minitest::Test
-  def test_handle_pull_request
-    assert_nil Bot.new.handle_pull_request!({})
+  def test_handle_pull_request_assign_pr_to_reviewer_in_body
+    client = Minitest::Mock.new
+    bot = Bot.new(client)
+
+    client.expect(:update_issue, true, ['rails/rails', 1234, { assignee: 'rafael_franca' }])
+
+    payload = {
+      'number' => 1234,
+      'repository' => {
+        'full_name' => 'rails/rails'
+      },
+      'pull_request' => {
+        'body' => 'r? @rafael_franca'
+      }
+    }
+
+    bot.handle_pull_request!(payload)
+
+    client.verify
   end
 end
