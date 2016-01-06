5b86c2e46310705db9f680dbfa53ffb20b78d580
Rafael Mendonça França <rafaelmfranca@gmail.com>
2016-01-06 02:10:08 -0200

    Add a nice comment to the PR is the author is new

--- lib/bot.rb
+++ lib/bot.rb
@@ -19,5 +19,31 @@ class Bot
     if assignee
       @client.update_issue(repository, number, assignee: assignee[:name])
     end
+
+    author = payload['pull_request']['user']['login']
+
+    if new_contributor?(repository, author)
+      @client.add_comment(repository, number, welcome_message(assignee[:name]))
+    end
+  end
+
+  private
+
+  # Internal: checks if the author is a new contributor.
+  def new_contributor?(repository, author)
+    contributors = @client.contributors(repository)
+
+    contributors.none? { |contributor| contributor['login'] == author }
+  end
+
+  # Internal: builds the welcome message.
+  def welcome_message(assignee)
+    <<~MSG % assignee
+    Thanks for the pull request, and welcome! The Rails team is excited to review your changes, and you should hear from @%s (or someone else) soon.
+
+    If any changes to this PR are deemed necessary, please add them as extra commits. This ensures that the reviewer can see what has changed since they last reviewed the code. Due to the way GitHub handles out-of-date commits, this should also make it reasonably obvious what issues have or haven't been addressed. Large or tricky changes may require several passes of review and changes.
+
+    Please see [the contribution instructions](http://edgeguides.rubyonrails.org/contributing_to_ruby_on_rails.html) for more information.
+    MSG
   end
 end
--- test/test_bot.rb
+++ test/test_bot.rb
@@ -5,6 +5,8 @@ class BotTest < Minitest::Test
     client = Minitest::Mock.new
     bot = Bot.new(client)
 
+    client.expect(:contributors, [{ 'login' => 'foo' }], ['rails/rails'])
+
     payload = build_payload
     payload['pull_request']['body'] = 'something'
 
@@ -17,7 +19,31 @@ class BotTest < Minitest::Test
     client = Minitest::Mock.new
     bot = Bot.new(client)
 
+    client.expect(:contributors, [{ 'login' => 'foo' }], ['rails/rails'])
     client.expect(:update_issue, true, ['rails/rails', 1234, { assignee: 'rafael_franca' }])
+
+    payload = build_payload
+
+    bot.handle_pull_request!(payload)
+
+    client.verify
+  end
+
+  def test_handle_pull_request_give_a_nice_comment_when_first_time_contributor
+    client = Minitest::Mock.new
+    bot = Bot.new(client)
+
+    message = <<~MSG
+    Thanks for the pull request, and welcome! The Rails team is excited to review your changes, and you should hear from @rafael_franca (or someone else) soon.
+
+    If any changes to this PR are deemed necessary, please add them as extra commits. This ensures that the reviewer can see what has changed since they last reviewed the code. Due to the way GitHub handles out-of-date commits, this should also make it reasonably obvious what issues have or haven't been addressed. Large or tricky changes may require several passes of review and changes.
+
+    Please see [the contribution instructions](http://edgeguides.rubyonrails.org/contributing_to_ruby_on_rails.html) for more information.
+    MSG
+
+    client.expect(:contributors, [], ['rails/rails'])
+    client.expect(:update_issue, true, ['rails/rails', 1234, { assignee: 'rafael_franca' }])
+    client.expect(:add_comment, true, ['rails/rails', 1234, message])
 
     payload = build_payload
 
@@ -35,7 +61,10 @@ class BotTest < Minitest::Test
         'full_name' => 'rails/rails'
       },
       'pull_request' => {
-        'body' => 'r? @rafael_franca'
+        'body' => 'r? @rafael_franca',
+        'user' => {
+          'login' => 'foo'
+        }
       }
     }
   end
